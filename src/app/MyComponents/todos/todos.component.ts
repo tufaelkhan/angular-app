db1d7557b43029f07447933419a9889910ffcac8
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-23 11:33:30 -0700

    add todo

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -31,8 +31,8 @@ export class TodosComponent implements OnInit {
   }
   toggleTodo(todo: Todo){
     console.log(todo);
-    // const index = this.todos.indexOf(todo)
-    this.todos.push(todo)
+    const index = this.todos.indexOf(todo)
+    this.todos[index].active = !this.todos[index].active;
     localStorage.setItem("todos", JSON.stringify(this.todos))
   }
 }
