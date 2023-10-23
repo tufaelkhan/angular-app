79edf18adece32dea444c131146d5fad643b45da
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-23 03:49:41 -0700

    add nav item

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
-export class TodoItemComponent {
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  constructor(){}
 
+  ngOnInit(): void {
+    
+  }
 }
