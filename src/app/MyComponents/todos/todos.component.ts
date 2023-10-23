37c45a79a077d7173f2b6a3f41be0c8edefd6a7f
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-23 06:59:17 -0700

    add todo

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -30,4 +30,9 @@ export class TodosComponent implements OnInit {
     ]
   }
   ngOnInit(): void{}
+  deleteTodo(todo: Todo){
+    console.log(todo);
+    const index = this.todos.indexOf(todo)
+    this.todos.splice(index, 1)
+  }
 }
