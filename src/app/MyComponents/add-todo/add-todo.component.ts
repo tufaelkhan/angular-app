f0f6e57be51cb2cd73247619fc6f785299da2a63
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-23 07:52:00 -0700

    add new

--- src/app/MyComponents/add-todo/add-todo.component.ts
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-add-todo',
@@ -6,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-todo.component.css']
 })
 export class AddTodoComponent implements OnInit {
-  
+  title: string;
+  desc: string;
+  @Output() todoAdd: EventEmitter<Todo> = new EventEmitter();
   constructor(){}
   ngOnInit(): void {
     
+  }
+  onSubmit(){
+    const todo = {
+      sno: 7,
+      title: this.title,
+      desc: this.desc,
+      active: true
+    }
+    this.todoAdd.emit(todo)
   }
 }
--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -35,4 +35,9 @@ export class TodosComponent implements OnInit {
     const index = this.todos.indexOf(todo)
     this.todos.splice(index, 1)
   }
+  addTodo(todo: Todo){
+    console.log(todo);
+    // const index = this.todos.indexOf(todo)
+    this.todos.push(todo)
+  }
 }
