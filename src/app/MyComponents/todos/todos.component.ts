5ef27330e2c26466bb85de355427d1c691007308
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-23 08:23:39 -0700

    add style

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -6,38 +6,27 @@ import {Todo} from '../../Todo'
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  localItem: string;
   todos: Todo[];
   constructor(){
-    this.todos = [
-      {
-        sno: 1,
-        title: "This is Title",
-        desc: "description",
-        active: true
-      },
-      {
-        sno: 2,
-        title: "This is Title2",
-        desc: "description",
-        active: true
-      },
-      {
-        sno: 3,
-        title: "This is Title3",
-        desc: "description",
-        active: true
-      }
-    ]
+    localItem = localStorage.getItem("todos")
+    if(this.localItem == null){
+      this.todos = [ ]
+    }else{
+      this.todos = JSON.parse(this.localItem)
+    }
   }
   ngOnInit(): void{}
   deleteTodo(todo: Todo){
     console.log(todo);
     const index = this.todos.indexOf(todo)
     this.todos.splice(index, 1)
+    localStorage.setItem("todos", JSON.stringify(this.todos))
   }
   addTodo(todo: Todo){
     console.log(todo);
     // const index = this.todos.indexOf(todo)
     this.todos.push(todo)
+    localStorage.setItem("todos", JSON.stringify(this.todos))
   }
 }
